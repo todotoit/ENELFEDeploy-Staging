3697e7702f79dc8213322ee49f61aaa97272ed8d
marcoaimo <marcoaimo.am@gmail.com>
2018-05-16 12:14:57 +0200

    deploy

--- app/main.js
+++ app/main.js
@@ -3921,7 +3921,7 @@ window.twttr = (function(d, s, id) {
     .value('appUrl', 'http://formulae.enel.com/app')
     .value('gameUrl', 'http://formulae.enel.com/game')
     .value('currentSeason', {id: 's4'})
-    .value('showcaseRace', {id: 'r8'})
+    .value('showcaseRace', {id: 'r9'})
 
 }(window.angular));
 
